bdabc5dfdb0dd9b60df7fab0429ef9f9872b2a0b
Skylar Anderson - iMac <skylar@skylaranderson.net>
2012-02-06 08:50:46 -0500

    Wrapped oSuccess in a timeout to simulate the async nature of the success callback.  Wrapped library in a closure, fixed a typo and validated against jsLint

--- jquery.devstash.js
+++ jquery.devstash.js
@@ -1,120 +1,128 @@
-var DevStash = {
+(function() {
 
-  getCacheItem: function(key) {
+  "use strict";
 
-    if(localStorage && localStorage.getItem && localStorage.getItem(key) && localStorage.loadFromCache) {
+  var DevStash = {
 
-      this.log("--[ Loading From Cache ]-- " + key);
-      this.log(JSON.parse(localStorage.getItem(key)));
+    getCacheItem: function(key) {
 
-      return JSON.parse(localStorage.getItem(key));  
+      if(localStorage && localStorage.getItem && localStorage.getItem(key) && localStorage.loadFromCache) {
 
-    } else {
+        this.log("--[ Loading From Cache ]-- " + key);
+        this.log(JSON.parse(localStorage.getItem(key)));
 
-      return false;
-
-    }
-
-  },
-  setCacheItem: function(key, obj) {
-
-    if(localStorage && localStorage.setItem && localStorage.writeToCache) {
-      
-      this.log("--[ Saving To Cache ]-- " + key);
-      this.log(obj);
-
-      localStorage.setItem(key, JSON.stringify(obj));  
-
-    }
-
-  },
-  capture: function(debug) {
-
-    localStorage.debugCache = debug;
-
-    delete localStorage.loadFromCache;
-    localStorage.writeToCache = true;
-
-    location.reload(true);      
-
-  },
-  load: function(debug) {
-
-    localStorage.debugCache = debug;
-
-    delete localStorage.writeToCache;
-    localStorage.loadFromCache = true;
-
-    location.reload(true);      
-
-  },
-  loadFromCache: function() {
-    return localStorage && localStorage.loadFromCache;
-  },
-  writeToCache: function() {
-    return localStorage && localStorage.writeToCache;
-  },
-  cleanKey: function(k) {
-    return k.replace(/.*\/rest/, '/rest');
-  },
-  log: function(stuff) {
-    if(console && console.log && localStorage && localStorage.debugCache) {
-      console.log(stuff);
-    }
-  }
-
-
-};
-
-var aj = $.ajax; 
-
-$.ajax = function(options) {
-
-  var key = DevStash.cleanKey(options.url);
-  var oSuccess = options.success;
-  var oBeforeSend = options.beforeSend;
-
-  var newOptions = {
-
-    beforeSend: function() {
-      
-      if(DevStash.loadFromCache() && DevStash.getCacheItem(key)) {
-
-        var successData = DevStash.getCacheItem(key);
-
-        oSuccess(successData.data, successData.textStatus, successData.jqXHR);          
-        return false;
+        return JSON.parse(localStorage.getItem(key));  
 
       } else {
 
-        return typeof oBeforeSend === 'function' ? o.beforeSend.apply(this, arguments) : true;
+        return false;
 
       }
-          
+
     },
-    success: function(data, textStatus, jqXHR) {
-      
-      if(DevStash.writeToCache()) {
+    setCacheItem: function(key, obj) {
 
-        DevStash.setCacheItem(key, {
-          data: data,
-          textStatus:textStatus,
-          jqXHR: jqXHR
-        });  
+      if(localStorage && localStorage.setItem && localStorage.writeToCache) {
 
-      }
+        this.log("--[ Saving To Cache ]-- " + key);
+        this.log(obj);
 
-      if(typeof oSuccess === 'function') {
-
-        oSuccess.apply(this, arguments);          
+        localStorage.setItem(key, JSON.stringify(obj));  
 
       }
 
+    },
+    capture: function(debug) {
+
+      localStorage.debugCache = debug;
+
+      delete localStorage.loadFromCache;
+      localStorage.writeToCache = true;
+
+      location.reload(true);      
+
+    },
+    load: function(debug) {
+
+      localStorage.debugCache = debug;
+
+      delete localStorage.writeToCache;
+      localStorage.loadFromCache = true;
+
+      location.reload(true);      
+
+    },
+    loadFromCache: function() {
+      return localStorage && localStorage.loadFromCache;
+    },
+    writeToCache: function() {
+      return localStorage && localStorage.writeToCache;
+    },
+    cleanKey: function(k) {
+      return k.replace(/.*\/rest/, '/rest');
+    },
+    log: function(stuff) {
+      if(console && console.log && localStorage && localStorage.debugCache) {
+        console.log(stuff);
+      }
     }
+
+
   };
 
-  aj($.extend({}, options, newOptions));
-    
-}
+  var aj = $.ajax; 
 
-window.DevStash = DevStash;
+  $.ajax = function(options) {
+
+    var key = DevStash.cleanKey(options.url);
+    var oSuccess = options.success;
+    var oBeforeSend = options.beforeSend;
+
+    var newOptions = {
+
+      beforeSend: function() {
+
+        if(DevStash.loadFromCache() && DevStash.getCacheItem(key)) {
+
+          var successData = DevStash.getCacheItem(key);
+          setTimeout(function() {
+            oSuccess(successData.data, successData.textStatus, successData.jqXHR);          
+          }, 0);
+
+          return false;
+
+        } else {
+
+          return typeof oBeforeSend === 'function' ? oBeforeSend.apply(this, arguments) : true;
+
+        }
+
+      },
+      success: function(data, textStatus, jqXHR) {
+
+        if(DevStash.writeToCache()) {
+
+          DevStash.setCacheItem(key, {
+            data: data,
+            textStatus:textStatus,
+            jqXHR: jqXHR
+          });  
+
+        }
+
+        if(typeof oSuccess === 'function') {
+
+          oSuccess.apply(this, arguments);          
+
+        }
+
+      }
+    };
+
+    aj($.extend({}, options, newOptions));
+
+  };
+
+  window.DevStash = DevStash;
+  
+}());
